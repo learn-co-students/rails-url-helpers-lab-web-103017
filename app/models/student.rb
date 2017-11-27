fc7e8b97a71e8857cddf04e3d9979c7615cf0b9c
George Gore <georgelgore@gmail.com>
2017-11-27 16:34:26 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+
+  def activate_student_page
+    if !self.active?
+      self.active = "true"
+    elsif self.active?
+      self.active = "false"
+    end
+  end
+
 end
