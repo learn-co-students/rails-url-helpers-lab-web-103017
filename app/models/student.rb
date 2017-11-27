a177082bf69362496c7abc4d00b83e84c23afd2c
Jake <jakemacnaughton@gmail.com>
2017-11-27 17:34:57 -0500

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate_student
+    if !self.active
+      self.active == true
+    else
+      self.active == false
+    end
+  end
+
 end
